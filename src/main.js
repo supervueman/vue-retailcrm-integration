8402054eb860bb66a6fe9ee74d6725f58055bf0f
supervueman <supervueman@gmail.com>
2020-02-17 13:41:41 +0400

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
+
+axios.create({
+  baseURL: process.env.VUE_APP_BASE_URL,
+  timeout: 1000,
+  headers: {
+    'X-Custom-Header': 'foobar'
+  }
+});
 
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount('#app');
